c5c41e17497c66a28377c1e1c5e8082171ca7cc0
Iskander Kudashev <grinerplaydark@gmail.com>
2023-02-15 17:46:38 +0300

    revert all

--- utils/index.ts
+++
@@ -1,8 +0,0 @@
-import fs from 'fs';
-
-export const getLectures = () => {
-  const lectures = fs.readdirSync('./docs/lectures');
-  return lectures
-    .filter((lecture) => lecture !== 'index.md')
-    .map(lecture => lecture.replace('.md', ''));
-}
